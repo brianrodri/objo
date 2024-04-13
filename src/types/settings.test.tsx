f8e09edb2c8e144d816f5e3f618538de9bdfcc98
Brian Rodriguez <brian@brianrodri.com>
2024-04-12 20:59:19 -0400

    test: check type of DEFAULT_SETTINGS

--- src/types/settings.test.tsx
+++ src/types/settings.test.tsx
@@ -1,6 +1,7 @@
-import { expect, test } from "vitest";
-import { DEFAULT_SETTINGS } from "./settings";
+import { expect, expectTypeOf, test } from "vitest";
+import { DEFAULT_SETTINGS, ObjoPluginSettings } from "./settings";
 
 test("default settings", () => {
     expect(DEFAULT_SETTINGS).toBeDefined();
+    expectTypeOf(DEFAULT_SETTINGS).toEqualTypeOf<ObjoPluginSettings>();
 });
