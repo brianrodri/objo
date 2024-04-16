6b8656415d1579a11b668fc70383cc49b213465f
Brian Rodriguez <brian@brianrodri.com>
2024-04-15 23:32:52 -0400

    docs: remove redundant JSDoc

--- src/plugin.tsx
+++ src/plugin.tsx
@@ -9,23 +9,19 @@ export default class ObjoPlugin extends Plugin {
         await this.loadSettings();
     }
 
-    /** Returns the plugin settings. */
     public getSettings(): ObjoPluginSettings {
         return this.settings;
     }
 
-    /** Updates the plugin settings. */
     public overwriteSettings(changes: Partial<ObjoPluginSettings>): void {
         this.settings = { ...this.settings, ...changes };
     }
 
-    /** Asynchronously loads the plugin settings from Obsidian's configuration storage. */
     public async loadSettings(): Promise<void> {
         const data = await this.loadData();
         this.settings = { ...DEFAULT_SETTINGS, ...data };
     }
 
-    /** Asynchronously saves the plugin settings to Obsidian's configuration storage. */
     public async saveSettings(): Promise<void> {
         await this.saveData(this.settings);
     }
