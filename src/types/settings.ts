113de2da3e01936f196fd1c839a56d84aa1b0a76
Brian Rodriguez <brian@brianrodri.com>
2024-06-05 12:48:34 -0400

    feat: fill-out default settings based on my personal vault

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,3 +1,39 @@
-export interface ObjoPluginSettings {}
+import { DateTimeUnit } from "luxon";
 
-export const DEFAULT_SETTINGS: ObjoPluginSettings = {};
+interface PeriodicLogConfig {
+    folder: string;
+    fileNameFormat: string;
+    unit: DateTimeUnit;
+    header?: boolean;
+    breadcrumbs?: boolean;
+}
+
+export interface ObjoPluginSettings {
+    periodic_logs: PeriodicLogConfig[];
+}
+
+export const DEFAULT_SETTINGS: ObjoPluginSettings = {
+    periodic_logs: [
+        {
+            unit: "day",
+            folder: "01-Logs/01-Daily",
+            fileNameFormat: "yyyy-MM-dd",
+            header: true,
+            breadcrumbs: true,
+        },
+        {
+            unit: "week",
+            folder: "01-Logs/02-Weekly",
+            fileNameFormat: "YYYY 'W'WW",
+            header: true,
+            breadcrumbs: true,
+        },
+        {
+            unit: "month",
+            folder: "01-Logs/03-Monthly",
+            fileNameFormat: "yyyy-MM",
+            header: true,
+            breadcrumbs: true,
+        },
+    ],
+};
