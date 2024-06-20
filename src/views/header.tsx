7eb8af4e97a68419cec848f440ebbb57648474ee
Brian Rodriguez <brianrz@amazon.com>
2024-06-20 15:29:51 -0400

    fix(header): fix detection of today/yesterday/tomorrow

--- src/views/header.tsx
+++ src/views/header.tsx
@@ -1,5 +1,4 @@
 import { DateTime, DurationLike } from "luxon";
-import { useCallback } from "preact/compat";
 
 import { PeriodicLog } from "@/collections/periodicLog";
 import { useObjoContext } from "@/compat/pluginContext";
@@ -9,26 +8,22 @@ const REFRESH_INTERVAL: DurationLike = { minutes: 1 };
 
 export function Header() {
     const { collection } = useObjoContext();
-    const getNow = useCallback(
-        () => DateTime.now().startOf(collection.unit).plus(collection.offset),
-        [collection.unit],
-    );
-    const now = useInterval(getNow, REFRESH_INTERVAL);
+    const now = useInterval(DateTime.now, REFRESH_INTERVAL);
 
     return <h1>{getHeaderContent(now, collection)}</h1>;
 }
 
-function getHeaderContent(now: DateTime, collection: PeriodicLog) {
-    const { interval, unit, label = unit, fileNameDateFormat, headerDateFormat = fileNameDateFormat } = collection;
-
-    switch (interval.start.diff(now, unit).as(unit)) {
-        case -1:
-            return unit === "day" ? "Yesterday" : `Last ${label}`;
-        case 0:
-            return unit === "day" ? "Today" : `This ${label}`;
-        case 1:
-            return unit === "day" ? "Tomorrow" : `Next ${label}`;
-        default:
-            return interval.start.toFormat(headerDateFormat);
+function getHeaderContent(
+    now: DateTime,
+    { interval, duration, unit, label = unit, fileNameDateFormat, headerDateFormat = fileNameDateFormat }: PeriodicLog,
+) {
+    if (interval.contains(now)) {
+        return unit === "day" ? "Today" : `This ${label}`;
+    } else if (interval.mapEndpoints((dt) => dt.minus(duration)).contains(now)) {
+        return unit === "day" ? "Tomorrow" : `Next ${label}`;
+    } else if (interval.mapEndpoints((dt) => dt.plus(duration)).contains(now)) {
+        return unit === "day" ? "Yesterday" : `Last ${label}`;
+    } else {
+        return interval.start.toFormat(headerDateFormat);
     }
 }
