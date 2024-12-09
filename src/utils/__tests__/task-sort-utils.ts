4ad9b5bc75d945a4b3a44239ed5c37794903e6d2
Brian Rodriguez <brian@brianrodri.com>
2024-12-09 14:37:26 -0500

    test: fix flakiness of DateTime sorting logic

--- src/utils/__tests__/task-sort-utils.ts
+++ src/utils/__tests__/task-sort-utils.ts
@@ -5,36 +5,42 @@ import { Task } from "@/data/task";
 import { byStartTime } from "@/utils/task-sort-utils";
 import { sortBy } from "lodash";
 
+const TIME_INVALID = DateTime.invalid("uh-oh!");
+const TIME_1300 = DateTime.fromISO("13:00");
+const TIME_0900 = DateTime.fromISO("09:00");
+const TIME_1630 = DateTime.fromISO("16:30");
+const TIME_2000 = DateTime.fromISO("20:00");
+
 describe("Sorting tasks", () => {
     it("should sort by start time", () => {
         const unorderedTasks: DeepPartial<Task>[] = [
-            { times: { start: DateTime.fromISO("13:00") } },
-            { times: { start: DateTime.fromISO("09:00") } },
-            { times: { start: DateTime.fromISO("20:00") } },
-            { times: { start: DateTime.fromISO("16:30") } },
+            { times: { start: TIME_1300 } },
+            { times: { start: TIME_0900 } },
+            { times: { start: TIME_2000 } },
+            { times: { start: TIME_1630 } },
         ];
 
         expect(sortBy(unorderedTasks, byStartTime)).toEqual([
-            { times: { start: DateTime.fromISO("09:00") } },
-            { times: { start: DateTime.fromISO("13:00") } },
-            { times: { start: DateTime.fromISO("16:30") } },
-            { times: { start: DateTime.fromISO("20:00") } },
+            { times: { start: TIME_0900 } },
+            { times: { start: TIME_1300 } },
+            { times: { start: TIME_1630 } },
+            { times: { start: TIME_2000 } },
         ]);
     });
 
     it("should place invalid starts at the end", () => {
         const unorderedTasks: DeepPartial<Task>[] = [
-            { times: { start: DateTime.invalid("uh-oh!") } },
-            { times: { start: DateTime.fromISO("13:00") } },
-            { times: { start: DateTime.fromISO("09:00") } },
-            { times: { start: DateTime.fromISO("20:00") } },
+            { times: { start: TIME_INVALID } },
+            { times: { start: TIME_1300 } },
+            { times: { start: TIME_0900 } },
+            { times: { start: TIME_2000 } },
         ];
 
         expect(sortBy(unorderedTasks, byStartTime)).toEqual([
-            { times: { start: DateTime.fromISO("09:00") } },
-            { times: { start: DateTime.fromISO("13:00") } },
-            { times: { start: DateTime.fromISO("20:00") } },
-            { times: { start: DateTime.invalid("uh-oh!") } },
+            { times: { start: TIME_0900 } },
+            { times: { start: TIME_1300 } },
+            { times: { start: TIME_2000 } },
+            { times: { start: TIME_INVALID } },
         ]);
     });
 });
