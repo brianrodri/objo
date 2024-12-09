00239e5cee7aac4b01ab8742c0ee29d2aa934b9a
Brian Rodriguez <brian@brianrodri.com>
2024-12-09 17:55:23 -0500

    chore: remove unused code

--- src/utils/date-time-utils.ts
+++
@@ -1,8 +0,0 @@
-import { DateTime } from "luxon";
-
-export function setTime(date: DateTime, time: DateTime): DateTime {
-    if (date.isValid && time.isValid) {
-        return date.startOf("day").plus(time.diff(time.startOf("day")));
-    }
-    return date;
-}
