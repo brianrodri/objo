b4e17944d30806dce6dd22297a50096f08c888aa
Brian Rodriguez <brian@brianrodri.com>
2025-03-01 10:28:35 -0500

    refactor: remove required "id" argument from collections

--- src/model/collection/periodic-log.ts
+++ src/model/collection/periodic-log.ts
@@ -1,12 +1,10 @@
 import assert from "assert";
-import { kebabCase } from "lodash";
 import { DateTime, DateTimeOptions, Duration, DurationLike, Interval, IntervalMaybeValid } from "luxon";
 import { parse } from "path";
 
 import { Collection } from "./schema";
 
 export class PeriodicLog extends Collection {
-    public readonly id: string;
     public readonly folder: string;
     public readonly dateFormat: string;
     public readonly dateOffset: Duration<true>;
@@ -14,7 +12,6 @@ export class PeriodicLog extends Collection {
     public readonly dateOptions: DateTimeOptions;
 
     public constructor(
-        id: string,
         folder: string,
         dateFormat: string,
         intervalDurationLike: DurationLike,
@@ -23,18 +20,15 @@ export class PeriodicLog extends Collection {
     ) {
         super();
 
-        id = id.trim();
         folder = folder.trim();
         const dateOffset = Duration.fromDurationLike(dateOffsetLike);
         const intervalDuration = Duration.fromDurationLike(intervalDurationLike);
 
-        assert(id.length > 0, "id must be non-empty");
         assert(folder.length > 0, "folder must be non-empty");
         assert(dateFormat.length > 0, "dateFormat must be non-empty");
         assert(dateOffset.isValid, "dateOffset must be valid");
         assert(intervalDuration.isValid && intervalDuration.valueOf() !== 0, "intervalDuration must be non-zero");
 
-        this.id = kebabCase(id);
         this.folder = folder;
         this.dateFormat = dateFormat;
         this.dateOffset = dateOffset;
@@ -49,11 +43,11 @@ export class PeriodicLog extends Collection {
     public override getIntervalOf(filePath: string): IntervalMaybeValid {
         const path = parse(filePath);
         if (path.dir !== this.folder) {
-            return Interval.invalid(`invalid ${this.id} folder`, `"${filePath}" is not in "${this.folder}"`);
+            return Interval.invalid(`invalid interval folder`, `"${filePath}" is not in "${this.folder}"`);
         }
         const date = DateTime.fromFormat(path.name, this.dateFormat, this.dateOptions);
         if (!date.isValid) {
-            return Interval.invalid(`invalid ${this.id} filename`, `${date.invalidReason} ${date.invalidExplanation}`);
+            return Interval.invalid(`invalid interval filename`, `${date.invalidReason} ${date.invalidExplanation}`);
         }
         return Interval.after(date.plus(this.dateOffset), this.intervalDuration);
     }
