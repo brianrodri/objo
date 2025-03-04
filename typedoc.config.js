1a1f8c53501d42745f7e0d07cfcd4e91507cda3a
Brian Rodriguez <brian@brianrodri.com>
2025-03-04 12:16:13 -0500

    build: fix baseUrl of typescript config (#26)

---
+++ typedoc.config.js
@@ -0,0 +1,6 @@
+export default {
+    entryPoints: ["./src/"],
+    entryPointStrategy: "expand",
+    exclude: ["./docs/**/*", "./node_modules/**/*", "**/__tests__/**/*", "**/*.const.ts"],
+    plugin: ["typedoc-plugin-coverage", "typedoc-github-theme"],
+};
