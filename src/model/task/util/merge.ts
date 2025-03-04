9ef82dea76479e62e554ca5fe8b9d1f0ef21cfdd
Brian Rodriguez <brian@brianrodri.com>
2025-03-04 17:06:00 -0500

    docs: install eslint-plugin-jsdoc and fix all errors (#27)

--- src/model/task/util/merge.ts
+++ src/model/task/util/merge.ts
@@ -5,10 +5,21 @@ import { DeepPartial } from "utility-types";
 import { DEFAULT_PRIORITY_VALUE, DEFAULT_TYPE_VALUE, TASK_WITH_DEFAULT_VALUES } from "@/model/task/constants";
 import { Task } from "@/model/task/schema";
 
+/**
+ * @param parts - the task pieces to merge.
+ * @returns a new {@link Task} with values taken from the front-most non-default {@link parts} encountered.
+ * @see {@link TASK_WITH_DEFAULT_VALUES}
+ */
 export function mergeTaskParts(...parts: DeepPartial<Task>[]): Task {
     return parts.reduce(replaceDefaultValues, { ...TASK_WITH_DEFAULT_VALUES });
 }
 
+/**
+ * Replaces any default values in {@link task} with non-default values from {@link part}.
+ * @param task - the task to modify.
+ * @param part - the task piece to take from.
+ * @returns the modified {@link Task}.
+ */
 function replaceDefaultValues(task: Task, part: DeepPartial<Task>): Task {
     return _.mergeWith(task, part, (oldValue, newValue, propName) => {
         if (propName === "type" && _.isString(oldValue) && _.isString(newValue)) {
