87f0cd85f9da2fa487aea1f879adb4e6c4ebbbca
Brian Rodriguez <brian@brianrodri.com>
2025-03-05 06:27:29 -0500

    docs: change navigation style (#34)

--- typedoc.config.js
+++ typedoc.config.js
@@ -7,4 +7,7 @@ export default {
     plugin: ["typedoc-plugin-coverage", "@typhonjs-typedoc/typedoc-theme-dmt", "typedoc-plugin-dt-links"],
     theme: "default-modern",
     exclude: ["./docs/**/*", "./node_modules/**/*", "**/__tests__/**/*"],
+
+    // https://github.com/typhonjs-typedoc/typedoc-theme-dmt#configuration
+    dmtNavigation: { style: "flat" },
 };
