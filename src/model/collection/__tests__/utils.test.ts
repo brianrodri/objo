b5e8d7f7ca0136163ef43c3309ac6407ef790803
Brian Rodriguez <brian@brianrodri.com>
2025-03-05 12:55:53 -0500

    feat: strip trailing slashes from folders when applicable

---
+++ src/model/collection/__tests__/utils.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from "vitest";
+
+import { sanitizeFolder } from "../utils";
+
+describe(`${sanitizeFolder.name}`, () => {
+    it("should strip trailing slashes", () => {
+        expect(sanitizeFolder("foo/")).toEqual("foo");
+    });
+
+    it("should do nothing if there are no trailing slashes", () => {
+        expect(sanitizeFolder("foo")).toEqual("foo");
+    });
+
+    it("should do nothing when folder is the vault root", () => {
+        expect(sanitizeFolder("/")).toEqual("/");
+    });
+});
