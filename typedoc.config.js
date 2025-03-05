e3081e237af1698f9ce83c4d615a3b121d9aa8f2
Brian Rodriguez <brian@brianrodri.com>
2025-03-05 04:47:35 -0500

    build: enforce 100% doc coverage

--- typedoc.config.js
+++ typedoc.config.js
@@ -2,6 +2,8 @@
 export default {
     entryPoints: ["./src/"],
     entryPointStrategy: "expand",
+    treatWarningsAsErrors: true,
+    treatValidationWarningsAsErrors: true,
     plugin: ["typedoc-plugin-coverage", "typedoc-github-theme", "typedoc-plugin-dt-links"],
     exclude: ["./docs/**/*", "./node_modules/**/*", "**/__tests__/**/*"],
 };
