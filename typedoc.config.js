6f311331a5c5f92efd5f94db9c9b2091390942c3
Brian Rodriguez <brian@brianrodri.com>
2025-03-06 00:52:22 -0500

    docs: record architecture decisions (#46)
    * docs: keep markdown docs in source control
    
    * docs: record architecture decisions
    
    * chore: enforce markdownlint on *.md files
    
    * docs: deploy ADRs to GitHub Pages

--- typedoc.config.js
+++ typedoc.config.js
@@ -2,11 +2,13 @@
 export default {
     entryPoints: ["./src/"],
     entryPointStrategy: "expand",
+    cleanOutputDir: false,
     treatWarningsAsErrors: true,
     treatValidationWarningsAsErrors: true,
     plugin: ["typedoc-plugin-coverage", "@typhonjs-typedoc/typedoc-theme-dmt", "typedoc-plugin-dt-links"],
     theme: "default-modern",
     exclude: ["./docs/**/*", "./node_modules/**/*", "**/__mocks__/**/*", "**/__tests__/**/*", "**/*.const.ts"],
+    projectDocuments: ["docs/architecture/decisions/*.md"],
 
     // https://github.com/typhonjs-typedoc/typedoc-theme-dmt#configuration
     dmtNavigation: { style: "flat" },
