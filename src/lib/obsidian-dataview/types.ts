0691d94daa1e24615acb00029e1de1c64b811545
Brian Rodriguez <brian@brianrodri.com>
2025-03-08 21:11:18 -0500

    style: sort members

--- src/lib/obsidian-dataview/types.ts
+++ src/lib/obsidian-dataview/types.ts
@@ -1,13 +1,15 @@
 import { DateTime } from "luxon";
-import { DataArray } from "obsidian-dataview/lib/api/data-array";
+import type { DataArray } from "obsidian-dataview/lib/api/data-array";
 import type { DataviewApi as ActualDataviewApi } from "obsidian-dataview/lib/api/plugin-api";
 import type { SMarkdownPage, STask } from "obsidian-dataview/lib/data-model/serialized/markdown";
 import type { Link as ActualLink } from "obsidian-dataview/lib/data-model/value";
 
 export { getAPI, isPluginEnabled } from "obsidian-dataview";
 
-/** The Obsidian 'link', used for uniquely describing a file, header, or block. Extended to improve type information. */
-export type Link = ActualLink;
+/** The API that the obsidian-dataview plugin exposes to plugin authors. Extended to improve type information. */
+export declare class DataviewApi extends ActualDataviewApi {
+    override pages(query: string): DataArray<SMarkdownPage>;
+}
 
 /** The metadata that the obsidian-dataview plugin extracts from tasks. Extended to improve type information. */
 export interface DataviewMarkdownTask extends STask {
@@ -19,7 +21,5 @@ export interface DataviewMarkdownTask extends STask {
     section: ActualLink;
 }
 
-/** The API that the obsidian-dataview plugin exposes to plugin authors. Extended to improve type information. */
-export interface DataviewApi extends ActualDataviewApi {
-    pages(query: string): DataArray<SMarkdownPage>;
-}
+/** The Obsidian 'link', used for uniquely describing a file, header, or block. Extended to improve type information. */
+export type Link = ActualLink;
