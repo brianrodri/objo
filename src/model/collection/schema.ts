b5c615b36f5513fa1d26443db7535412a4855a28
Brian Rodriguez <brian@brianrodri.com>
2025-03-08 21:15:43 -0500

    docs: fix broken type link

--- src/model/collection/schema.ts
+++ src/model/collection/schema.ts
@@ -9,7 +9,7 @@ export abstract class Collection {
     public abstract includes(filePath: string): boolean;
 }
 
-/** A {@link Collection} where each note corresponds to a unique {@link Interval} of time. */
+/** A {@link Collection} where each note corresponds to a unique {@link luxon.Interval} of time. */
 export abstract class DateBasedCollection extends Collection {
     /**
      * @param filePath - the path to check.
