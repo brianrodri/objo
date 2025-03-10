f6158005c47943c0ea4684754aaf82de5f9b43f5
Brian Rodriguez <brian@brianrodri.com>
2025-03-10 18:55:10 -0400

    test: fix typescript errors in strongly-typed assertValidLuxonValue

--- src/util/__tests__/luxon-utils.test.ts
+++ src/util/__tests__/luxon-utils.test.ts
@@ -1,5 +1,5 @@
-import { DateTime, Duration, Interval } from "luxon";
-import { describe, expect, it } from "vitest";
+import { DateTime, DateTimeMaybeValid, Duration, DurationMaybeValid, Interval, IntervalMaybeValid } from "luxon";
+import { describe, expect, expectTypeOf, it } from "vitest";
 
 import { assertValidDateTimeFormat, assertValidLuxonValue, mergeIntersecting } from "../luxon-utils";
 import { MERGE_INTERSECTING_TEST_CASES } from "./luxon-utils.test.const";
@@ -9,20 +9,41 @@ describe(`${assertValidLuxonValue.name}`, () => {
     const explanation = "user-provided explanation";
 
     describe.each(["user-provided message", undefined])("with message=%j", (message) => {
-        it.each([
-            DateTime.fromISO("2025-03-05"),
-            Duration.fromDurationLike({ days: 1 }),
-            Interval.after(DateTime.now(), { days: 1 }),
-        ])("should accept valid $constructor.name", (value) => {
-            expect(() => assertValidLuxonValue(value, message)).not.toThrow();
+        it("should accept valid datetimes", () => {
+            const value = DateTime.fromISO("2025-03-05");
+            expectTypeOf(value).toEqualTypeOf<DateTimeMaybeValid>();
+            assertValidLuxonValue(value, message);
+            expectTypeOf(value).toEqualTypeOf<DateTime<true>>();
         });
 
-        it.each([DateTime.fromISO("2025-03-99"), Duration.invalid(reason), Interval.invalid(reason, explanation)])(
-            "should reject invalid $constructor.name",
-            (value) => {
-                expect(() => assertValidLuxonValue(value, message)).toThrowErrorMatchingSnapshot();
-            },
-        );
+        it("should accept valid durations", () => {
+            const value = Duration.fromDurationLike({ days: 1 }) as DurationMaybeValid;
+            expectTypeOf(value).toEqualTypeOf<DurationMaybeValid>();
+            assertValidLuxonValue(value, message);
+            expectTypeOf(value).toEqualTypeOf<Duration<true>>();
+        });
+
+        it("should accept valid intervals", () => {
+            const value = Interval.after(DateTime.now(), { days: 1 });
+            expectTypeOf(value).toEqualTypeOf<IntervalMaybeValid>();
+            assertValidLuxonValue(value, message);
+            expectTypeOf(value).toEqualTypeOf<Interval<true>>();
+        });
+
+        it("should reject invalid datetimes", () => {
+            const value = DateTime.invalid(reason, explanation);
+            expect(() => assertValidLuxonValue(value, message)).toThrowErrorMatchingSnapshot();
+        });
+
+        it("should reject invalid durations", () => {
+            const value = Duration.invalid(reason, explanation);
+            expect(() => assertValidLuxonValue(value, message)).toThrowErrorMatchingSnapshot();
+        });
+
+        it("should reject invalid intervals", () => {
+            const value = Interval.invalid(reason, explanation);
+            expect(() => assertValidLuxonValue(value, message)).toThrowErrorMatchingSnapshot();
+        });
     });
 });
 
