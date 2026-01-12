a161a72440cd068430fcae608f25bd7e3b5db7dd
Brian Rodriguez <brian@brianrodri.com>
2026-01-12 14:34:09 -0500

    chore: remove type re-exports to prevent circular resolution

--- src/lib/obsidian/__mocks__/types.ts
+++ src/lib/obsidian/__mocks__/types.ts
@@ -1,7 +1,5 @@
 import { vi } from "vitest";
 
-export type { EventRef, MetadataCache, Pos, TAbstractFile, TFile } from "obsidian";
-
 export const App = vi.fn();
 export const Component = vi.fn();
 export const MarkdownRenderer = { render: vi.fn() };
